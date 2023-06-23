5aa2fc71e69a01396569a6ba72b18f05492f1c3d
rohanraj7 <rohanraj9645@gmail.com>
2023-06-24 01:01:15 +0530

    the saved like

--- frontend/users/src/components/SavedPost.jsx
+++ frontend/users/src/components/SavedPost.jsx
@@ -248,14 +248,14 @@ function SavedPost() {
                                                 </div>
                                                 <div className="" style={{ marginLeft: '150px' }}>
                                                     {vLikes?.some((like) => like.user === user.user_id && save.user.id === like.user) ? (
-                                                        <button onClick={() => { unlike_video(save.user.id) }} className="flex gap-2 items-center">
+                                                        <button onClick={() => { unlike_video(save.video.id) }} className="flex gap-2 items-center">
                                                             <svg xmlns="http://www.w3.org/2000/svg" fill="red" viewBox="0 0 24 24" strokeWidth={1.5} stroke="none" className="w-6 h-6">
                                                                 <path strokeLinecap="round" strokeLinejoin="round" d="M21 8.25c0-2.485-2.099-4.5-4.688-4.5-1.935 0-3.597 1.126-4.312 2.733-.715-1.607-2.377-2.733-4.313-2.733C5.1 3.75 3 5.765 3 8.25c0 7.22 9 12 9 12s9-4.78 9-12z" />
                                                             </svg>
                                                         </button>
                                                     ) : (
                                                         // If there is no match, render a button with a "liked" action
-                                                        <button onClick={() => { like_video(user.user_id, save.user.id) }} className="flex gap-2 items-center">
+                                                        <button onClick={() => { like_video(user.user_id, save.video.id) }} className="flex gap-2 items-center">
                                                             <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
                                                                 <path strokeLinecap="round" strokeLinejoin="round" d="M21 8.25c0-2.485-2.099-4.5-4.688-4.5-1.935 0-3.597 1.126-4.312 2.733-.715-1.607-2.377-2.733-4.313-2.733C5.1 3.75 3 5.765 3 8.25c0 7.22 9 12 9 12s9-4.78 9-12z" />
                                                             </svg>
